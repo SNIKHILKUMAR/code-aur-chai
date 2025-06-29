ed1762e639e19d025b310e499f9969d668995ddd
SNIKHILKUMAR <133903280+SNIKHILKUMAR@users.noreply.github.com>
2025-06-30 01:07:40 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,13 @@
+public class p1 {
+    public static void main(String[] args) {
+        int marks[] =new int[3];
+        marks[0]=56;
+        marks[1]=89;
+        marks[2]=57;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);
+            
+        } 
+    }
+    
+}
